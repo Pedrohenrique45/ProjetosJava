18f4a0b6c0bda66d072caac51670dd07f9a61197
Pedro Henrique <122459206+Pedrohenrique45@users.noreply.github.com>
2024-04-24 21:03:56 -0300

    alguns exercicios do curso

--- PrimeiroProjeto/src/Exercicios/ex005.java
+++ PrimeiroProjeto/src/Exercicios/ex005.java
@@ -1,7 +1,27 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class ex005 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Primeira nota: ");
+        double nota1 = scan.nextDouble();
+
+        System.out.println("Segunda nota: ");
+        double nota2 = scan.nextDouble();
+
+        double media  = (nota1 + nota2)/2;
+
+        if(media == 10){
+            System.out.println("Parabéns você foi aprovado com distinção!!");
+        }else if (media >= 6){
+            System.out.println("Parbéns você foi aprovado!!");
+        }else if (media < 6 && media >=4) {
+            System.out.println("Reprovado, mas pode fazer prova final");
+        }else {
+            System.out.println("Reprovado");
+        }
     }
 }
---
+++ PrimeiroProjeto/src/Exercicios/ex006.java
@@ -0,0 +1,18 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class ex006 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Numero 1: ");
+        double numero1 = scan.nextDouble();
+        System.out.print("Numero 2: ");
+        double numero2 = scan.nextDouble();
+        System.out.print("Numero 3: ");
+        double numero3 = scan.nextDouble();
+
+
+    }
+}
