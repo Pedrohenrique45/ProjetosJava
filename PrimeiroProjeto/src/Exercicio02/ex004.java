914ddbc527695c66c4c5cf9e3a1824372f1b98a5
Pedro Henrique <122459206+Pedrohenrique45@users.noreply.github.com>
2024-04-30 13:43:20 -0300

    uplade

---
+++ PrimeiroProjeto/src/Exercicio02/ex004.java
@@ -0,0 +1,13 @@
+package Exercicio02;
+
+public class ex004 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+}
